bf7b8cf18ff2372f10f4e918f58c0306ebf2f5ab
AudreyGl <ad.gl@protonmail.com>
2019-05-25 20:10:34 +0200

    Ajout contrib en cours

--- agoraviz/public/javascripts/global.js
+++ agoraviz/public/javascripts/global.js
@@ -3,12 +3,16 @@ var debatsListData = [];
 
 // DOM Ready =============================================================
 $(document).ready(function() {
+  
   // Populate the debat table on initial page load
   populateTable();
 // Question link click
   $('#debatsList table tbody').on('click', 'td a.linkshowdebat', showDebatInfo);
+  
     // Delete Debat link click
   $('#debatsList table tbody').on('click', 'td a.linkdeletedebat', deleteDebat);
+
+
   
 
 });
@@ -44,7 +48,7 @@ function showDebatInfo( event ) {
   // Prevent Link from Firing
   event.preventDefault();
 
-  // Retrieve username from link rel attribute
+  // Retrieve question from link rel attribute
   var thisDebatQuestion = $(this).attr('rel');
   // Get Index of object based on id value
   var arrayPosition = debatsListData.map(function(arrayItem) { return arrayItem.question; }).indexOf(thisDebatQuestion);
@@ -53,6 +57,13 @@ function showDebatInfo( event ) {
 
   //Populate Info Box
   $('#debatInfoQuestion').text(thisDebatObject.question);
+  $('#debatId').text(thisDebatObject._id);
+  $('#debatInfoContribs').text('');
+  //thisDebatObject.reponses.forEach(function())
+  //$('#debatInfoContribs').text(thisDebatObject.reponses.type+''+thisDebatObject.reponses.tcourt+'\n'+thisDebatObject.reponses.tlong+thisDebatObject.reponses.auteur+thisDebatObject.reponses.date);
+
+  $('#formAddContrib').style.display = "block";
+  $('#inputContribQuestionId').value = thisDebatObject._id;
 
 };
 
@@ -148,4 +159,66 @@ function deleteDebat(event) {
 
   }
 
+};
+
+
+
+  $('#btnAddContrib').on('click', addContrib);
+
+// Add Contrib
+function addContrib(event) {
+  event.preventDefault();
+
+  // Super basic validation - increase errorCount variable if any fields are blank
+  var errorCount = 0;
+  $('#formAddContrib input').each(function(index, val) {
+
+    if($(this).val() === '') { errorCount++; }
+  });
+
+  // Check and make sure errorCount's still at zero
+  if(errorCount === 0) {
+
+  questionParentId = $('#formAddContrib form input#inputContribQuestionId').val();
+
+    // If it is, compile all user info into one object
+    var newContrib = {
+      "questionParent" :  questionParentId,
+      "contribParent" : $('#formAddContrib form input#inputDebatQuestion').val(),
+        "type" : document.querySelector('input[name="contribType"]:checked').value,
+        "tcourt" : $('#formAddContrib form input#inputContribTexteCourt').val(),
+        "tlong" : $('#formAddContrib form input#inputContribTexteLong').val(),
+        "auteur" : $('#formAddContrib form input#inputContribAuteur').val()
+        //"timestamp" : + new Date();
+    }
+
+    // Use AJAX to post the object to our adddebat service
+    $.ajax({
+      type: 'POST',
+      data: newContrib,
+      url: '/debats/'+ questionParentId + '/addcontrib',
+      dataType: 'JSON'
+    }).done(function( response ) {
+        console.log( newContrib );
+      // Check for successful (blank) response
+      if (response.msg === '') {
+
+        // Clear the form inputs
+        $('#formAddContrib form input').val('');
+
+
+      }
+      else {
+
+        // If something goes wrong, alert the error message that our service returned
+        alert('Error: ' + response.msg);
+
+      }
+    });
+  }
+  else {
+    // If errorCount is more than 0, error out
+    alert('Please fill in all fields');
+    return false;
+  }
 };
--- agoraviz/routes/debats.js
+++ agoraviz/routes/debats.js
@@ -11,6 +11,17 @@ router.get('/debatslist', function(req, res) {
 
 });
 
+/* GET debatid. */
+router.get('/debatslist/:id', function(req, res) {
+  var db = req.db;
+  var collection = db.get('debatcollection');
+
+  var debatToFocusOn = req.params.id;
+  collection.find({ '_id' : debatToFocusOn }, function(e,docs){
+    res.json(docs);
+  });
+
+});
 
 
 
@@ -26,3 +37,73 @@ router.delete('/deletedebat/:id', function(req, res) {
 });
 
 module.exports = router;
+
+
+/* POST to Add Contrib Service */
+router.post('/:id/addcontrib', function(req, res) {
+console.log(req);
+    // Set our internal DB variable
+    var db = req.db;
+    console.log(req);
+    // Get our form values. These rely on the "name" 
+
+     
+     var contribQuestionId = req.params.id;
+    
+    var contribParent = null;
+    var contribType = req.body.contribType;
+    var contribTexteCourt = req.body.tcourt;
+    var contribTexteLong = req.body.tlong;
+    var contribAuteur = req.body.auteur;
+
+    var dateNewContrib = new Date();
+    var jour = dateNewContrib.getDate();
+    var mois = dateNewContrib.getMonth()+1;
+    var annee = dateNewContrib.getFullYear();
+    var heure = dateNewContrib.getHours();
+    var minute = dateNewContrib.getMinutes();
+    var seconde = dateNewContrib.getSeconds();
+
+    var contribDateHeure =jour+'/'+mois+'/'+annee+':'+heure+'H'+minute+'min'+seconde+'s';
+
+    // Set our collection
+    var collection = db.get('contribcollection');
+
+    // Submit to the DB
+    collection.insert({
+    	"questionParent" : contribQuestionId,
+    	"contribParent" : contribParent,
+        "type" : contribType,
+        "tcourt" : contribTexteCourt,
+        "tlong" : contribTexteLong,
+        "auteur" : contribAuteur,
+        "timestamp" : contribDateHeure
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.redirect("debatslist");
+        }
+    });
+
+
+
+});
+
+
+router.get('/debatslist/:id', function(req, res) {
+    var db = req.db;
+    var collection = db.get('debatcollection');
+    collection.find({},{},function(e,docs){
+        res.render('pages/debatslist', {
+            "debatslist" : docs,
+            title: 'AgoraViz'
+        });
+    });
+    res.redirect("debat");
+});
+
+
