e6eb5f612e94e52695effa04474a5a1b45b67c52
cbrizard <clement.brizard@etu.utc.fr>
2019-06-02 23:58:46 +0200

    Enabled graph construction

--- agoraviz/public/javascript/radialTreeMap.js
+++ agoraviz/public/javascript/radialTreeMap.js
@@ -1,53 +1,3 @@
-/* === Dataset === */
-
-databis = [
-  {"name": "Eve",   "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": ""},
-  {"name": "Seth",  "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Eve"},
-  {"name": "Cain",  "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Eve"},
-  {"name": "Enos",  "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Seth"},
-  {"name": "Noam",  "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Seth"},
-  {"name": "Ninon",  "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Seth"},
-  {"name": "Ninon1",  "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Seth"},
-  {"name": "Ninon2",  "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Seth"},
-  {"name": "Ninon3",  "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Seth"},
-  {"name": "Albert",  "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Seth"},
-  {"name": "Philémon",  "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Eve"},
-  {"name": "Audrey", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Philémon"},
-  {"name": "Audrey1", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Philémon"},
-  {"name": "Audrey2", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Philémon"},
-  {"name": "Audrey3", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Philémon"},
-  {"name": "Awan",  "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Eve"},
-  {"name": "Audrey4", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Awan"},
-  {"name": "Audrey5", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Awan"},
-  {"name": "Audrey6", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Awan"},
-  {"name": "Audrey7", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Awan"},
-  {"name": "Audrey8", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Awan"},
-  {"name": "Audrey9", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Awan"},
-  {"name": "Audrey10", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Awan"},
-  {"name": "Audrey11", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Awan"},
-  {"name": "Audrey12", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Awan"},
-  {"name": "Audrey13", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Awan"},
-  {"name": "Audrey14", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Awan"},
-  {"name": "Audrey15", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Awan"},
-  {"name": "Clément", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Eve"},
-  {"name": "Alien", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Clément"},
-  {"name": "Alien1", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Clément"},
-  {"name": "Alien2", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Clément"},
-  {"name": "Alien3", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Clément"},
-  {"name": "Alien4", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Clément"},
-  {"name": "Alien5", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Clément"},
-  {"name": "Alien6", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Clément"},
-  {"name": "Alien7", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Cain"},
-  {"name": "Alien8", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Cain"},
-  {"name": "Alien9", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Cain"},
-  {"name": "Alien10", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Cain"},
-  {"name": "Alien11", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Cain"},
-  {"name": "Alien12", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Cain"},
-  {"name": "Alien13", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Cain"},
-  {"name": "Alien14", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Cain"},
-  {"name": "Alien15", "value":"Dies iræ, dies illa, dies tribulationis et angustiæ, dies calamitatis et miseriæ, dies tenebrarum et caliginis, dies nebulæ et turbinis, dies tubæ et clangoris super civitates munitas et super angulos excelsos.", "parent": "Cain"}
-]
-
 var data = contributions;
 const debateJSON = debate;
 
@@ -62,7 +12,7 @@ var svg = d3.select("svg"),
 
 
 var stratify = d3.stratify()
-    .id(function(d) { return d.name; })
+    .id(function(d) { return d._id; })
     .parentId(function(d) { return d.parent; })
 
 
@@ -72,45 +22,45 @@ var tree = d3.tree()
 
 var root = tree(stratify(data));
 
-  // Arcs
+// Arcs
 
-  var link = g.selectAll(".link")
-    .data(root.descendants().slice(1))
-    .enter().append("path")
-        .attr("class", "link")
-        .style("stroke", "#8da0cb")
-        .attr("d", function(d) {
-          return "M" + project(d.x, d.y)
-              + "C" + project(d.x, (d.y + d.parent.y)  /2)
-              + " " + project(d.parent.x, (d.y + d.parent.y) / 2)
-              + " " + project(d.parent.x, d.parent.y);
-        });
-
-
-  // Noeuds
-
-  var node = g.selectAll(".node")
-    .data(root.descendants())
-    .enter().append("g")
-      .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
-      .attr("transform", function(d) { return "translate(" + project(d.x, d.y) + ")"; })
-
-  node.append("circle")
-        .attr("r", 4.5)
-        .on('click', click)
-        .on("mouseover", handleMouseOver)
-        .on("mouseout", handleMouseOut);
+var link = g.selectAll(".link")
+  .data(root.descendants().slice(1))
+  .enter().append("path")
+      .attr("class", "link")
+      .style("stroke", "#8da0cb")
+      .attr("d", function(d) {
+        return "M" + project(d.x, d.y)
+            + "C" + project(d.x, (d.y + d.parent.y)  /2)
+            + " " + project(d.parent.x, (d.y + d.parent.y) / 2)
+            + " " + project(d.parent.x, d.parent.y);
+      });
 
 
+// Noeuds
 
-  // Labels
+var node = g.selectAll(".node")
+  .data(root.descendants())
+  .enter().append("g")
+    .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
+    .attr("transform", function(d) { return "translate(" + project(d.x, d.y) + ")"; })
 
-  node.append("text")
-      .attr("dy", ".31em")
-      .attr("x", function(d) { return d.x < 180 === !d.children ? 6 : -6; })
-      .style("text-anchor", function(d) { return d.x < 180 === !d.children ? "start" : "end"; })
-      .attr("transform", function(d) { return "rotate(" + (d.x < 180 ? d.x - 90 : d.x + 90) + ")"; })
-      .text(function(d) { return d.id.substring(d.id.lastIndexOf(".") + 1); });
+node.append("circle")
+      .attr("r", 4.5)
+      .on('click', click)
+      .on("mouseover", handleMouseOver)
+      .on("mouseout", handleMouseOut);
+
+
+
+// Labels
+
+node.append("text")
+    .attr("dy", ".31em")
+    .attr("x", function(d) { return d.x < 180 === !d.children ? 6 : -6; })
+    .style("text-anchor", function(d) { return d.x < 180 === !d.children ? "start" : "end"; })
+    .attr("transform", function(d) { return "rotate(" + (d.x < 180 ? d.x - 90 : d.x + 90) + ")"; })
+    .text(function(d) { return d.id.substring(d.id.lastIndexOf(".") + 1); });
 
 // Tooltips
 
@@ -137,28 +87,6 @@ $("#addNode").click(function() {
     url: '/contributions/',
     dataType: 'JSON'
   });
-
-  var newNodeObj = {
-    type: 'resource-delete',
-    name: $("#label").val(),
-    attributes: [],
-    children: [],
-    value: $("#comment").val()
-  };
-
-  var newNode = d3.hierarchy(newNodeObj);
-  newNode.depth = selected.depth + 1;
-  newNode.height = selected.height - 1;
-  newNode.parent = selected;
-  newNode.id =$("#label").val(); // label
-
-  if(!selected.children){
-    selected.children = [];
-    selected.data.children = [];
-  }
-  selected.children.push(newNode);
-  update(selected);
-
 });
 
 
