b23c960f4ec37ee98259ffb1338245c64cb9be35
ninon <lizeninon@yahoo.fr>
2019-06-12 16:24:05 +0200

    Stat temporel

--- agoraviz/models/contribution.js
+++ agoraviz/models/contribution.js
@@ -24,8 +24,51 @@ exports.getByDate = (db, debate, end, cb) => {
 	      : cb(null, docs);
 	  });
 	};
+	
+	
+	exports.countByDay = (db, debate, cb) => {
+	    const debateId = debate;
+	    db.get('contribcollection').aggregate(
+	        [{
+	            $group: {
+	                _id: {
+	                    month: {
+	                        $month: "$timestamp"
+	                    },
+	                    day: {
+	                        $dayOfMonth: "$timestamp"
+	                    },
+	                    year: {
+	                        $year: "$timestamp"
+	                    }
+	                },
+	                count: {
+	                    $sum: 1
+	                }
+	            }
+	        }], (err, docs) => {
+	            return err ?
+	                cb(err) :
+	                cb(null, docs);
+	        })
+	};
 
 
+	/*
+	 * 
+	 * db.sales.aggregate(
+   [
+      {
+        $group : {
+           _id : { month: { $month: "$date" }, day: { $dayOfMonth: "$date" }, year: { $year: "$date" } },
+           totalPrice: { $sum: { $multiply: [ "$price", "$quantity" ] } },
+           averageQuantity: { $avg: "$quantity" },
+           count: { $sum: 1 }
+        }
+      }
+   ]
+)
+	 */
 // Create a new contribution
 exports.new = (db, obj, cb) => {
   db.get('contribcollection').insert({
