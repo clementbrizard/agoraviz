b8f889c082917a17e3be619122a97daa7e9855fd
Ninon Lize-Masclef <ninon.lize-masclef@etu.utc.fr>
2019-06-13 19:19:36 +0200

    Update radialTreeMap.js

--- agoraviz/public/javascript/radialTreeMap.js
+++ agoraviz/public/javascript/radialTreeMap.js
@@ -3,105 +3,93 @@ selectedForSynthese = [];
 data.push({"_id" : debate._id, "parent" : "", "name" : debate.question, "value":""});
 contributions.forEach(function(c){data.push(c)});
 const debateJSON = debate;
-console.log(contributions);
-console.log(data);
-createGraph(data);
 
 }
 
+var tree = d3.tree()
+.size([360, 500])
+.separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
+
+
+var stratify = d3.stratify()
+    .id(function(d) { return d._id; })
+    .parentId(function(d) { return d.parent; })
+
+createGraph(data);
+ 
 /* === Création du graphe === */
 
 function createGraph(data){
 
-  color = d3.scaleOrdinal()
-  .domain(["ouicar", "ouimais", "noncar","nonmais"])
-  .range(["green", "yellow", "orange", "red"])
-  .unknown("white");
+	color = d3.scaleOrdinal()
+	.domain(["ouicar", "ouimais", "noncar","nonmais","synthetise"])
+	.range(["green", "yellow", "orange", "red","grey"])
+	.unknown("white");
  
-  var svg = d3.select("svg"),
-      width = +svg.attr("width"),
-      height = +svg.attr("height"),
-      g = svg.append("g")
-        .attr("transform", "translate(" + (width / 2 + 40) + "," + (height /3) + ")");
-  
-  
-  var stratify = d3.stratify()
-      .id(function(d) { return d._id; })
-      .parentId(function(d) { return d.parent; })
-  
-  
-  var tree = d3.tree()
-      .size([360, 500])
-      .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
-  
-  
-  var root = tree(stratify(data));
-  
-  // Arcs
-  
-  var link = g.selectAll(".link")
-    .data(root.descendants().slice(1))
-    .enter().append("path")
-        .attr("class", "link")
-        .style("stroke", "#8da0cb")
-        .attr("d", function(d) {
-          return "M" + project(d.x, d.y)
-              + "C" + project(d.x, (d.y + d.parent.y)  /2)
-              + " " + project(d.parent.x, (d.y + d.parent.y) / 2)
-              + " " + project(d.parent.x, d.parent.y);
-        });
-  
-  
-  // Noeuds
-  
-  var node = g.selectAll(".node")
-    .data(root.descendants())
-    .enter().append("g")
-      .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
-      .attr("transform", function(d) { return "translate(" + project(d.x, d.y) + ")"; })
-  
-  node.append("circle")
-        .attr("r", 4.5)
-        .style('fill', function(d) { return color(d.data.type)})
-        .style("stroke",  function(d) { return d.data.parent==""? "#8da0cb" : null})
-        .on('click', click)
-        .on("mouseover", handleMouseOver)
-        .on("mouseout", handleMouseOut);
-  
-  
-  
-  // Labels
-  
-  node.append("text")
-      .attr("dy", ".31em")
-      .attr("x", function(d) { return d.x < 180 === !d.children ? 6 : -6; })
-      .style("text-anchor", function(d) { return d.x < 180 === !d.children ? "start" : "end"; })
-      .attr("transform", function(d) { return "rotate(" + (d.x < 180 ? d.x - 90 : d.x + 90) + ")"; })
-      .text(function(d) { return d.data.name.substring(d.data.name.lastIndexOf(".") + 1); });
+	var svg = d3.select("svg"),
+	    width = +svg.attr("width"),
+	    height = +svg.attr("height"),
+	    g = svg.append("g")
+	      .attr("transform", "translate(" + (width / 2 + 40) + "," + (height /3) + ")");
+	
+	
+	var root = tree(stratify(data));
+	
+	// Arcs
+	
+	var link = g.selectAll(".link")
+	  .data(root.descendants().slice(1))
+	  .enter().append("path")
+	      .attr("class", "link")
+	      .style("stroke", "#8da0cb")
+	      .attr("d", function(d) {
+	        return "M" + project(d.x, d.y)
+	            + "C" + project(d.x, (d.y + d.parent.y)  /2)
+	            + " " + project(d.parent.x, (d.y + d.parent.y) / 2)
+	            + " " + project(d.parent.x, d.parent.y);
+	      });
+	
+	
+	// Noeuds
+	
+	var node = g.selectAll(".node")
+	  .data(root.descendants())
+	  .enter().append("g")
+	    .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
+	    .attr("transform", function(d) { return "translate(" + project(d.x, d.y) + ")"; })
+	
+	node.append("circle")
+	      .attr("r", 4.5)
+	      .style('fill', function(d) { return color(d.data.type)})
+	      .style("stroke",  function(d) { return d.data.parent==""? "#8da0cb" : null})
+	      .on('click', click)
+	      .on("mouseover", handleMouseOver)
+	      .on("mouseout", handleMouseOut);
+	
 
-  
-  $.getScript("https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js", function()
-      {
+	
+	$.getScript("https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js", function()
+			{
 
-         d3.selectAll("svg .node").each(function(d, i){
-           $(this).popover({
-           container: 'body' ,
-           title: 'Bonjour',
-           placement: 'bottom',
-           content: d.data.value
-           });
-        });
+				 d3.selectAll("svg .node").each(function(d, i){
+					 $(this).popover({
+					 container: 'body' ,
+					 title: 'Bonjour',
+					 placement: 'bottom',
+					 content: d.data.value
+					 });
+				});
 
-         // quand on clique ailleurs de la popover ça l'enlève
-    /*
-         $('body').on('click', function (e) {
-                if ($(e.target).data('toggle') !== 'popover'
-                    && $(e.target).parents('svg .node').length === 0
-                    && $(e.target).parents('.popover.in').length === 0) {
-                    $('svg .node').popover('hide');
-                }
-            });  */
-      });
+				 // quand on clique ailleurs de la popover ça l'enlève
+		/*
+				 $('body').on('click', function (e) {
+				        if ($(e.target).data('toggle') !== 'popover'
+				            && $(e.target).parents('svg .node').length === 0
+				            && $(e.target).parents('.popover.in').length === 0) {
+				            $('svg .node').popover('hide');
+				        }
+				    });  */
+			});
 
 
 
@@ -119,91 +107,99 @@ var div = d3.select("body").append("div")
 var selected=null;
 $("#addNode").click(function() {
 
-    let parent = "";
-    if (data.length == 0) {
-      parent = debateJSON._id;
-    } else {
-      parent = selected.data._id;
-    }
+	 console.log("parent"); 
 
-    const newContrib = {
-      debate: debateJSON._id,
-      parent: parent,
-      type: $("#type").val(),
-      name: $("#label").val(),
-      value: $("#comment").val(),
-      auteur: $("#auteur").val()
-    };
+	  let parent = "";
+	  if (data.length == 0) {
+	    parent = debateJSON._id;
+	  } else {
+	    parent = selected.data._id;
+	  }
 
-    $.ajax({
-      type: 'POST',
-      data: newContrib,
-      url: '/contributions/',
-      dataType: 'text',
-    }).done(function(response) {
+	  console.log("parent", parent); 
+	  
+	  const newContrib = {
+	    debate: debateJSON._id,
+	    parent: parent,
+	    type: $("#type").val(),
+	    name: $("#label").val(),
+	    value: $("#comment").val(),
+	    auteur: $("#auteur").val()
+	  };
+
+	  $.ajax({
+	    type: 'POST',
+	    data: newContrib,
+	    url: '/contributions/',
+	    dataType: 'text',
+	  }).done(function(response) {
 
 
-      var newNodeObj = {
-            type: $("#type").val(),
-            name: $("#label").val(),
-            attributes: [],
-            children: [],
-            value: $("#comment").val()
-          };
-          var newNode = d3.hierarchy(newNodeObj);
-          newNode.depth = selected.depth + 1;
-          newNode.height = selected.height - 1;
-          newNode.parent = selected;
-          newNode.id =$("#label").val(); // label
+		  var newNodeObj = {
+				    type: $("#type").val(),
+				    name: $("#label").val(),
+				    attributes: [],
+				    children: [],
+				    value: $("#comment").val()
+				  };
+				  var newNode = d3.hierarchy(newNodeObj);
+				  newNode.depth = selected.depth + 1;
+				  newNode.height = selected.height - 1;
+				  newNode.parent = selected;
+				  newNode.id =$("#label").val(); // label
 
-          if(!selected.children){
-            selected.children = [];
-            selected.data.children = [];
-          }
-          selected.children.push(newNode);
-          update(selected);
+				  if(!selected.children){
+				    selected.children = [];
+				    selected.data.children = [];
+				  }
+				  selected.children.push(newNode);
+				  
+				  console.log("selected", selected);
+				  location.reload(true);
+//				  update(selected);
 
-    });
-  });
+	  }); 
+	  
+	});
 
 
 
 $("#addSynthese").click(function() {
 
-    const newSynthese = {
-      description: $("#description").val(),
-      contributions: JSON.stringify(selectedForSynthese),
-      debate: debateJSON._id,
-      auteur: $("#auteursynthese").val(),
-    };
+	  const newSynthese = {
+	    description: $("#description").val(),
+	    contributions: JSON.stringify(selectedForSynthese),
+	    debate: debateJSON._id,
+	    auteur: $("#auteursynthese").val(),
+	  };
 
-    console.log("selectedForSynthese:"+newSynthese.contributions);
+	  console.log("selectedForSynthese:"+newSynthese.contributions);
 
-    $.ajax({
-      type: 'POST',
-      data: newSynthese,
-      url: '/syntheses/',
-      dataType: 'text',
-    }).done(function(response) {
-      location.reload(true);
-    });
-  });
+	  $.ajax({
+	    type: 'POST',
+	    data: newSynthese,
+	    url: '/syntheses/',
+	    dataType: 'text',
+	  }).done(function(response) {
+	    location.reload(true);
+	  });
+	});
 
 
 
 $("#end").click(function(){
-  $.ajax({
-      type: 'GET',
-      url: '/api/contributions/'+debateJSON._id+'/'+$("#dateValue").val(),
-      dataType: 'text',
-    }).done(function(response) {
-      data=JSON.parse(response);
-      data.push({"_id" : debate._id, "parent" : "", "name" : debate.question, "value":""})  
-      console.log(data);
-      $("svg").empty();
-      createGraph(data);
-      
-    });
+	$.ajax({
+	    type: 'GET',
+	    url: '/api/contributions/'+debateJSON._id+'/'+$("#dateValue").val(),
+	    dataType: 'text',
+	  }).done(function(response) {
+		  data=JSON.parse(response);
+		  data.push({"_id" : debate._id, "parent" : "", "name" : debate.question, "value":""})	
+		  console.log(data);
+		  $("svg").empty();
+		  createGraph(data);
+	    
+	  });
 })
 
 
@@ -241,8 +237,10 @@ function handleMouseOut(d) {
 function update(source) {
 
   var duration=700,
-  temp=d3.selectAll("path.link"),
-  treeData = tree(root);
+  temp=d3.selectAll("path.link");
+  var root = tree(stratify(data));
+  var treeData = tree(root);
+  var g = d3.select("svg").select("g");
 
   var nodes = treeData.descendants(),
   links = treeData.descendants().slice(1);
